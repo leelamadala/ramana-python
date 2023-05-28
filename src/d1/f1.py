5a240cbf915c70d6a8094faa92cd628d76aa13d2
leelaMadala <mlsankar@icloud.com>
2023-05-28 12:01:16 +0530

    init commit

---
+++ src/d1/f1.py
@@ -0,0 +1,29 @@
+# Created on : 16-May-2023, 8:52:19 pm
+# Author     : mlsankar
+
+
+# This is a sample Python script.
+
+# Press Shift+F6 to execute it or replace it with your code.
+
+
+def print_hi(name):
+    print(f"Hi, {name}")
+
+
+if __name__ == "__main__":
+    print_hi("PyCharm")
+    
+    print("Leela")
+    
+print("Welcome!")
+
+g = input("Guss the Number: ")
+guss = int(g)
+
+if guss == 5:
+    print("You win!")
+else:
+    print("You lose!")
+    
+print("Game Over!")
